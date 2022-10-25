df4afba327ed30a6e4cdce10f5ea57fef6e27092
Max <maxkorsv@gmail.com>
2022-10-25 20:13:15 +0300

    initial commit

---
+++ src/main/java/com/maxkors/librebnb/LibrebnbApplication.java
@@ -0,0 +1,13 @@
+package com.maxkors.librebnb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrebnbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrebnbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maxkors/librebnb/LibrebnbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxkors.librebnb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrebnbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
